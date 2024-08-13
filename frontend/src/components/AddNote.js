50eeec575e0e08b67a0b18fe2538fc1291730476
Ahmed Raza <129895725+Ahmed0Raza@users.noreply.github.com>
2024-08-13 15:53:21 +0500

    reload issue resolving..

--- frontend/src/components/AddNote.js
+++ frontend/src/components/AddNote.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import noteContext from "../context/notes/NoteContext";
 import Alert from "./Alert"; // Import the Alert component
 
